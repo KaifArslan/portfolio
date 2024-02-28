d607cd8bfd18aa117e8fba661d83422812611abd
Kaif Arslan <110412833+KaifArslan@users.noreply.github.com>
2024-02-28 18:44:48 +0530

    Update darkmode.js

--- src/scripts/darkmode.js
+++ src/scripts/darkmode.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.getElementById("checkbox")
+  document.body.classList.toggle("dark")
   checkbox.addEventListener("change", () => {
     document.body.classList.toggle("dark")
     console.log('testing this out')
